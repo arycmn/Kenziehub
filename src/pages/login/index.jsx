dbee3e6b637d46a907f5bf13b705b918836a9f94
Gabriel Fabiano <gabrielfabianoo91@gmail.com>
2020-12-09 17:13:21 -0300

    fix:thunk and applyMiddleware imported, token global and yup resolvers added

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,19 +1,29 @@
 import { useForm } from "react-hook-form";
 import { PageLogin, FormLogin, InfoLog, InputLog, ButtonLog } from "./styled";
 import { api } from "../../services/API";
+import { useSelector, useDispatch } from "react-redux";
+import { getTokenThunk } from "../../store/modules/token/thunks";
 
 const Login = () => {
   const { register, handleSubmit, _watch, _errors } = useForm();
+
+  const selector = useSelector((state) => state.token);
+  const dispatch = useDispatch();
 
   const handleForm = (data) => {
     api
       .post("/sessions", { ...data })
       .then((res) => {
         window.localStorage.setItem("token", res.data.token);
+        dispatch(getTokenThunk(res.data.token));
         //props.setAuth(true)
         //history.push("/users")
       })
       .catch((err) => console.log(err.response.data.message));
+  };
+
+  const mostraToken = () => {
+    console.log(selector);
   };
 
   return (
@@ -26,6 +36,7 @@ const Login = () => {
           <InputLog placeholder="Senha" ref={register} name="password" />
           <ButtonLog type="submit">Entrar</ButtonLog>
         </FormLogin>
+        <button onClick={mostraToken}>Button ttest token</button>
       </PageLogin>
     </>
   );
