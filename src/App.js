4e8da5a2282d42628448c6b1bd5893d48acfa875
Cláudio Siqueira <claudio.filho_rv@hotmail.com>
2020-12-14 18:24:25 -0300

    fix: removed <body>, causing unnecessary scrollbars

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import styled from "styled-components";
-import "./styles/index.css";
 import "reactjs-popup/dist/index.css";
 import Routes from "./routes";
 import GlobalStyle from "./styles/global";
@@ -7,24 +5,18 @@ import { allDevsThunk } from "./store/modules/allDevs/thunks";
 import { useDispatch } from "react-redux";
 import { useEffect } from "react";
 
-function App() {
+const App = () => {
   const dispatch = useDispatch();
-
   useEffect(() => {
     dispatch(allDevsThunk());
   }, []);
 
   return (
-    <Body>
+    <>
       <GlobalStyle />
       <Routes />
-    </Body>
+    </>
   );
-}
-
-const Body = styled.div`
-  width: 100vw;
-  height: 100vh;
-`;
+};
 
 export default App;
